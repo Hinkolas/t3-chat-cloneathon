cc0d19708b4596dfc0626328bcd7a39fe53e30de
Nicholas Hinke <hinke.nicholas@icloud.com>
2025-06-13 16:52:30 +0200

    added chat service to start command

--- service/cmd/start.go
+++ service/cmd/start.go
@@ -1,12 +1,17 @@
 package cmd
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	_ "github.com/mattn/go-sqlite3"
+
 	"github.com/Hinkolas/t3-chat-cloneathon/service/internal/application"
 	"github.com/Hinkolas/t3-chat-cloneathon/service/internal/application/auth"
+	"github.com/Hinkolas/t3-chat-cloneathon/service/internal/application/chat"
+	"github.com/Hinkolas/t3-chat-cloneathon/service/internal/llm"
 	"github.com/spf13/cobra"
 )
 
@@ -45,12 +50,58 @@ var startCmd = &cobra.Command{
 			json.NewEncoder(w).Encode(map[string]string{"status": "success", "message": "Why are you checking? Don't you trust me?"})
 		})
 
+		db, err := sql.Open("sqlite3", "./_data/dev.db")
+		if err != nil {
+			panic(err)
+		}
+
+		app.Database = db
+
 		authService, err := auth.NewService(app)
 		if err != nil {
 			fmt.Printf("Error initializing auth service: %v\n", err)
 		}
 
 		authService.Handle(app.Router)
+
+		// T3 Chat
+		chatService, err := chat.NewService(app)
+		if err != nil {
+			fmt.Printf("Error initializing auth service: %v\n", err)
+		}
+
+		chatService.AddModel("claude-4-sonnet", llm.Model{
+			Title:       "Claude 4 Sonnet",
+			Description: "The latest model from Anthropic. Claude 4 Sonnet is a significant upgrade to Claude 3.7 Sonnet, delivering superior coding and reasoning while responding more precisely to your instructions.",
+			Icon:        "anthropic",
+			Name:        "claude-4-opus-20250514",
+			Provider:    llm.Anthropic,
+			Features: llm.ModelFeatures{
+				HasVision: true,
+				HasPDF:    true,
+			},
+			Flags: llm.ModelFlags{
+				IsPremium:     true,
+				IsRecommended: true,
+			},
+		})
+
+		chatService.AddModel("qwen3", llm.Model{
+			Title:       "Qwen3",
+			Description: "An open source mixture-of-experts (MoE) language model developed by Alibaba Cloud, activating only 3 billion parameters out of a total of 30B. It comes in various sizes and is licenced under the Apache 2.0 license.",
+			Icon:        "qwen",
+			Name:        "qwen3:30b",
+			Provider:    llm.Ollama,
+			Features: llm.ModelFeatures{
+				HasReasoning: true,
+			},
+			Flags: llm.ModelFlags{
+				IsOpenSource: true,
+				IsFree:       true,
+			},
+		})
+
+		chatService.Handle(app.Router)
 
 		if err = app.Start(); err != nil {
 			fmt.Printf("Error starting the application: %v\n", err)
