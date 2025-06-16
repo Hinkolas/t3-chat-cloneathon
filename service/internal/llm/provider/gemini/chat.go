4dd0cac86165b78f109c342cdc5f753c2a82c5da
Nicholas Hinke <hinke.nicholas@icloud.com>
2025-06-16 15:00:53 +0200

    add support for thought summaries

--- service/internal/llm/provider/gemini/chat.go
+++ service/internal/llm/provider/gemini/chat.go
@@ -3,6 +3,8 @@ package gemini
 import (
 	"context"
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/Hinkolas/t3-chat-cloneathon/service/internal/llm/chat"
 	"github.com/Hinkolas/t3-chat-cloneathon/service/internal/llm/stream"
@@ -36,7 +38,22 @@ func StreamCompletion(req chat.Request) (*stream.Stream, error) {
 		}
 	}
 
-	chat, err := client.Chats.Create(ctx, req.Model, nil, messages)
+	config := genai.GenerateContentConfig{}
+
+	// Add temperature to the ollama request options
+	if req.Temperature >= 0 && req.Temperature <= 1 {
+		temp := float32(req.Temperature)
+		config.Temperature = &temp
+	}
+
+	if req.ReasoningEffort > 0 {
+		config.ThinkingConfig = &genai.ThinkingConfig{
+			IncludeThoughts: true,
+			ThinkingBudget:  &req.ReasoningEffort,
+		}
+	}
+
+	chat, err := client.Chats.Create(ctx, req.Model, &config, messages)
 	if err != nil {
 		return nil, err
 	}
@@ -47,8 +64,11 @@ func StreamCompletion(req chat.Request) (*stream.Stream, error) {
 
 		for result, err := range chat.SendMessageStream(ctx, genai.Part{Text: req.Messages[len(req.Messages)-1].Content}) {
 
+			content, thoughts := getChunk(result)
+
 			s.Publish(stream.Chunk{
-				Content: result.Text(),
+				Content:   content,
+				Reasoning: thoughts,
 			})
 
 			if err != nil {
@@ -66,5 +86,55 @@ func StreamCompletion(req chat.Request) (*stream.Stream, error) {
 	fmt.Println("Gemini stream started!") // TODO: Remove this debug statement
 
 	return s, nil
+
+}
+
+func getChunk(r *genai.GenerateContentResponse) (string, string) {
+
+	if len(r.Candidates) == 0 || r.Candidates[0].Content == nil || len(r.Candidates[0].Content.Parts) == 0 {
+		return "", ""
+	}
+
+	if len(r.Candidates) > 1 {
+		log.Println("Warning: there are multiple candidates in the response, returning text from the first one.")
+	}
+
+	var texts []string
+	var thoughts []string
+	var notTextParts []string
+	for _, part := range r.Candidates[0].Content.Parts {
+		if part.Text != "" {
+			if part.Thought {
+				thoughts = append(texts, part.Text)
+			} else {
+				texts = append(texts, part.Text)
+			}
+		} else {
+			if part.InlineData != nil {
+				notTextParts = append(notTextParts, "InlineData")
+			}
+			if part.CodeExecutionResult != nil {
+				notTextParts = append(notTextParts, "CodeExecutionResult")
+			}
+			if part.ExecutableCode != nil {
+				notTextParts = append(notTextParts, "ExecutableCode")
+			}
+			if part.FileData != nil {
+				notTextParts = append(notTextParts, "FileData")
+			}
+			if part.FunctionCall != nil {
+				notTextParts = append(notTextParts, "FunctionCall")
+			}
+			if part.FunctionResponse != nil {
+				notTextParts = append(notTextParts, "FunctionResponse")
+			}
+		}
+	}
+
+	if len(notTextParts) > 0 {
+		log.Printf("Warning: there are non-text parts %s in the response, returning concatenation of all text parts. Please refer to the non text parts for a full response from model.\n", strings.Join(notTextParts, ", "))
+	}
+
+	return strings.Join(texts, ""), strings.Join(thoughts, "")
 
 }
